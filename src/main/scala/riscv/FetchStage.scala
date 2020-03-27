ea63bc0b6bc667e67fa1e8501ed75a6fbc396e2f
E <evscamilo@gmail.com>
2020-03-27 18:39:50 +0100

    Updated fetch stage;

--- src/main/scala/riscv/FetchStage.scala
+++ src/main/scala/riscv/FetchStage.scala
@@ -8,20 +8,20 @@ package riscv
 import chisel3._
 import chisel3.util._
 
-class FetchStage extends Module {
+class FetchStage(data: Array[String] = Array()) extends Module {
   val io = IO(new Bundle {
     val pcSrc = Input(Bool())  // control signal from ID
-    val ifIdPc = Input(UInt(65.W)) // branch address from ID
+    val ifIdPc = Input(UInt(64.W)) // branch address from ID
     val pcWrite = Input(Bool())  // branch signal from hazard dt ID
-    val ifFlush = Input(Bool())  // control signal from main ctl ID @TODO: Check width
+    val ifFlush = Input(Bool())  // control signal from main ctl ID
     val ifIdWrite = Input(Bool())  // control signal from main ctl ID
 
-    val ifOut = Output(UInt(97.W)) // pip. reg, width: ifFlush (1 bit), instruction (32 bits), addr (64 bits),
+    val ifOut = Output(UInt(96.W)) // pip. reg, instruction (32 bits), addr (64 bits),
   })
 
   val pcRg = RegInit(0.asUInt(64.W)) // PC
-  val inMem = Module(new InstructionMemory()) // 32-bit rom instruction mem.
-  val ifRg = RegInit(0.asUInt(97.W)) // pipeline register
+  val inMem = Module(new InstructionMemory(data)) // 32-bit rom instruction mem.
+  val ifRg = RegInit(0.asUInt(96.W)) // pipeline register
 
   // pcWrite to branch
   // branch addr or increment addr
@@ -32,8 +32,16 @@ class FetchStage extends Module {
   // hazard ctl signal for stalls
   when (io.ifIdWrite) {
     // concatenating ifFlush, instruction and addr
-    ifRg := Cat(io.ifFlush, inMem.io.rdData, pcRg)
+    when (!io.ifFlush) {
+      //       Cat(MSB, LSB)
+      ifRg := Cat(pcRg, inMem.io.rdData)
+      val data = inMem.io.rdData
+      printf(p"data: $data, reg: $pcRg \n")
+    } .otherwise {
+      //       Cat(MSB, LSB)
+      ifRg := Cat(pcRg, 0.asUInt(32.W))
+      printf(p"FLUSHING data: 000s, reg: $pcRg \n")
+    }
   }
-
   io.ifOut := ifRg
 }
--- src/test/scala/riscv/FetchStageTest.scala
+++ src/test/scala/riscv/FetchStageTest.scala
@@ -9,26 +9,73 @@ object FetchStageTester {
 }
 
 class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
-  var adr1: UInt = 8.U
-  var adr2: UInt = 11.U
+
+  // incremental addressing
+  poke(r.io.pcSrc, false.B) // does not pick up branch
+  poke(r.io.ifIdPc, 0.U) // irrelevant
+  poke(r.io.pcWrite, true.B)
+  poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, false.B)
+
+  step(1)
+
+  expect(r.io.ifOut, 1.U)
 
   poke(r.io.pcSrc, false.B)
-  poke(r.io.ifIdPc, 2.U)
+  poke(r.io.ifIdPc, 0.U)
   poke(r.io.pcWrite, true.B)
-  poke(r.io.ifFlush, true.B)
   poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, false.B)
 
-  step(2)
+  step(1)
 
-  expect(r.io.ifOut, 0.U)
+  expect(r.io.ifOut, 17179869185L.U)
 
+  // flushing
+  poke(r.io.pcSrc, false.B)
+  poke(r.io.ifIdPc, 0.U)
+  poke(r.io.pcWrite, true.B)
+  poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, true.B)
+
+  step(1)
+
+  expect(r.io.ifOut, 34359738368L.U)
+
+  // incremental
+  poke(r.io.pcSrc, false.B)
+  poke(r.io.ifIdPc, 0.U)
+  poke(r.io.pcWrite, true.B)
+  poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, false.B)
+
+  step(1)
+
+  expect(r.io.ifOut, 51539607555L.U)
+
+  // branching
+  poke(r.io.pcSrc, true.B)
+  poke(r.io.ifIdPc, 0.U)
+  poke(r.io.pcWrite, true.B)
+  poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, false.B)
+
+  step(3)
+
+  expect(r.io.ifOut, 1.U)
 }
 
 class FetchStageTest extends FlatSpec with Matchers {
 
   "FetchStage" should "pass" in {
     iotesters.Driver.execute(FetchStageTester.param,
-      () => new FetchStage()) { c =>
+      () => new FetchStage(Array(
+        "b00000000000000000000000000000001", // 0
+        "b00000000000000000000000000000010",
+        "b00000000000000000000000000000011",
+        "b00000000000000000000000000000100",
+        "b00000000000000000000000000000101"
+      ))) { c =>
       new FetchStageTester(c)
     } should be(true)
   }
