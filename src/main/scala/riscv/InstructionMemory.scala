34fa75208e8f23c02e41a02c54421eb2946ba952
E <evscamilo@gmail.com>
2020-03-27 16:11:20 +0100

    Updated for stages and tests;

--- src/main/scala/riscv/InstructionMemory.scala
+++ src/main/scala/riscv/InstructionMemory.scala
@@ -11,16 +11,13 @@ class InstructionMemory(data: Array[String] = Array()) extends Module {
   val rom = VecInit(ar.map(_.U(32.W)))  // Array with 0s
   val calcAddr = RegInit(0.U(10.W))
 
-  calcAddr := io.rdAddr
-
   // load instructions into the memory
   if (!data.isEmpty) { for (i <- data.indices) { rom(i) := data(i).U } }
 
-  printf(p"BEFORE: ($calcAddr) ")
-
-  when (calcAddr > 0.U) { calcAddr := (calcAddr >> 2.U).asUInt() - 1.U }
-
-  printf(p"AFTER: ($calcAddr) ")
+  // ignoring the lower 2 bits in the address to match the 2-d matrix struct
+  when (io.rdAddr > 0.U) {
+    calcAddr := (io.rdAddr >> 2.U).asUInt()
+  }
 
   // read address
   io.rdData := rom(calcAddr.asUInt())
--- src/test/scala/riscv/InstructionMemoryTest.scala
+++ src/test/scala/riscv/InstructionMemoryTest.scala
@@ -9,21 +9,31 @@ object InstructionMemoryTester {
 }
 
 class InstructionMemoryTester(r: InstructionMemory) extends PeekPokeTester(r) {
-  var adr1: UInt = 0.U
-  var adr2: UInt = 4.U
-  var adr3: UInt = 8.U
+  var adr0: UInt = 0.U
+  var adr1: UInt = 4.U
+  var adr2: UInt = 8.U
+  var adr3: UInt = 12.U
+  var adr4: UInt = 16.U
 
-  poke(r.io.rdAddr, adr1)
-  step(2)
+  poke(r.io.rdAddr, adr0)
+  step(1)
   expect(r.io.rdData, "b101010110".U)
 
-  poke(r.io.rdAddr, adr2)
-  step(4)
+  poke(r.io.rdAddr, adr1)
+  step(3)
   expect(r.io.rdData, "b1101010101".U)
 
-  poke(r.io.rdAddr, adr3)
-  step(4)
+  poke(r.io.rdAddr, adr2)
+  step(3)
   expect(r.io.rdData, "b1101010111".U)
+
+  poke(r.io.rdAddr, adr3)
+  step(3)
+  expect(r.io.rdData, "b1111010111".U)
+
+  poke(r.io.rdAddr, adr4)
+  step(3)
+  expect(r.io.rdData, "b1001010111".U)
 
 }
 
@@ -31,7 +41,7 @@ class InstructionMemoryTest extends FlatSpec with Matchers {
 
   "InstructionMemory" should "pass" in {
     iotesters.Driver.execute(InstructionMemoryTester.param,
-      () => new InstructionMemory(Array("b101010110", "b1101010101", "b1101010111"))) { c =>
+      () => new InstructionMemory(Array("b101010110", "b1101010101", "b1101010111", "b1111010111", "b1001010111"))) { c =>
       new InstructionMemoryTester(c)
     } should be(true)
   }
