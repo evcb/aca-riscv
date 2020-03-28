5e3ee21751bba5b37f712d909276008bcaea4bb1
E <evscamilo@gmail.com>
2020-03-28 18:30:12 +0100

    Updated MemStage and WriteBackStage circuit design;

--- src/main/scala/riscv/MemStage.scala
+++ src/main/scala/riscv/MemStage.scala
@@ -10,9 +10,8 @@ import chisel3.util._
 
 class MemStage extends Module {
   val io = IO(new Bundle {
-    val exMemRg = Input(UInt(193.W))
+    val idExRg = Input(UInt(193.W))
 
-    // outputs
     val exMemRegWr = Output(Bool()) // EX_MEM_REGWRITE
     val memWb = Output(Bool()) // EX_MEM_Wb pass-through
     val memAddr = Output(Bool()) // EX_MEM_Addr pass-through
@@ -20,13 +19,13 @@ class MemStage extends Module {
     val memOut = Output(UInt(99.W)) // stage output
   })
 
-  val exMemWb = io.exMemRg(0) // EX_MEM_Wb, bool
-  val memWr = io.exMemRg(1)  // MemWrite, bool
-  val memRd = io.exMemRg(2)  // MemRead, bool
+  val exMemWb = io.idExRg(0) // EX_MEM_Wb, bool
+  val memWr = io.idExRg(1)  // MemWrite, bool
+  val memRd = io.idExRg(2)  // MemRead, bool
 
-  val exMemAddr = io.exMemRg(34, 2) // EX_MEM_ADDRESS, 32.W
-  val exMemWd = io.exMemRg(67, 35) // EX_MEM_WRITE_DATA, 32.W
-  val exMemRd = io.exMemRg(99, 67) // EX_MEM_Rd, 32.W
+  val exMemAddr = io.idExRg(34, 2) // EX_MEM_ADDRESS, 32.W
+  val exMemWd = io.idExRg(67, 35) // EX_MEM_WRITE_DATA, 32.W
+  val exMemRd = io.idExRg(99, 67) // EX_MEM_Rd, 32.W
 
   val dataMem = Module(new Memory())
   val memRg = RegInit(0.U(99.W))  // pipeline register 128 bits
--- src/main/scala/riscv/WriteBackStage.scala
+++ src/main/scala/riscv/WriteBackStage.scala
@@ -9,26 +9,23 @@ import chisel3._
 
 class WriteBackStage extends Module {
   val io = IO(new Bundle {
-    val memToRg = Input(Bool()) // MEM_TO_Reg
-    val memWbD = Input(UInt(64.W)) // MEM_WB_D
-    val memWbAddr = Input(UInt(64.W))  // MEM_WB_Addr
-    val exMemRegWr = Input(UInt(64.W))  // pass-through EX_MEM_RegWrite
-    val wbRd = Input(UInt(64.W))  // pass-through MEM_WB_Rd
+    val exMemRg = Input(UInt(99.W))
 
     val memWbRegWr = Output(UInt(64.W))  // pass-through MEM_WB_RegWrite
     val memWbRd = Output(UInt(64.W))  // pass-through MEM_WB_Rd
-    val wbStOut = Output(UInt(128.W))  // pass-through MEM_WB_Rd
-  })
 
-  val memWbData = RegInit(0.asSInt(128.W))
+    val wbOut = Output(UInt(128.W))
+  })
+  val memToRg = io.exMemRg(0) // MEM_TO_Reg, bool
+  val memWbD = io.exMemRg(33, 1) // MEM_WB_D, 32 bits
+  val memWbAddr = io.exMemRg(33, 1)  // MEM_WB_Addr, 32 bits
+  val exMemRegWr = io.exMemRg(33, 1)  // pass-through EX_MEM_RegWrite, 32 bits
+  val wbRd = io.exMemRg(33, 1)  // pass-through MEM_WB_Rd, 32 bits
 
   // if memToRg then memWbData
-  memWbData := Mux(io.memToRg, io.memWbD, io.memWbAddr)
-
-  // output
-  io.wbStOut := memWbData
+  io.wbOut := Mux(memToRg, memWbD, memWbAddr) // output
 
   // pass-through
-  io.memWbRegWr := io.exMemRegWr
-  io.memWbRd := io.wbRd
+  io.memWbRegWr := exMemRegWr
+  io.memWbRd := wbRd
 }
