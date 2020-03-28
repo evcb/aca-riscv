1421574e91f3909aab0c32aec27d94570faf78cf
E <evscamilo@gmail.com>
2020-03-28 14:26:28 +0100

    Fixed a bug in InstructionMemory where address 0 could not be accessed after other addresses had been accessed;

--- src/main/scala/riscv/InstructionMemory.scala
+++ src/main/scala/riscv/InstructionMemory.scala
@@ -9,21 +9,15 @@ class InstructionMemory(data: Array[String] = Array()) extends Module {
   })
   val ar = Array.fill(1024)(0)
   val rom = VecInit(ar.map(_.U(32.W)))  // Array with 0s
-  val calcAddr = RegInit(0.U(10.W))
 
   // load instructions into the memory
   if (!data.isEmpty) { for (i <- data.indices) { rom(i) := data(i).U } }
 
-  printf(p"addr data $io \n")
-
   // addresses must be multiple of 4
   // ignoring lower 2 bits in the address to match the 2-d matrix struct
   when (io.rdAddr > 0.U) {
-    calcAddr := (io.rdAddr >> 2.U).asUInt()
+    io.rdData := (io.rdAddr >> 2.U).asUInt()
+  } .otherwise {
+    io.rdData := rom(io.rdAddr)
   }
-
-  printf(p"address given $calcAddr \n")
-
-  // read address
-  io.rdData := rom(calcAddr)
 }
