23858bb736e0ed2e65c1c99a76da15c77db84c95
mariya <s193275@student.dtu.dk>
2020-03-31 14:00:33 +0200

    take constants out of Alu

--- src/main/scala/riscv/Alu.scala
+++ src/main/scala/riscv/Alu.scala
@@ -3,49 +3,26 @@ package riscv
 import chisel3.{Wire, _}
 import chisel3.util._
 
-/**
- * Constants - size of wires, etc.
- * TODO: consider moving to own file - does anyone else need to use these?
- */
-object Alu
-{
-  // Sizes of wires
-  def SZ_ALU_FN = UInt(2.W) // size of the ALU operation signal
-  def SZ_INPUT = UInt(64.W) // size of an input
-  def SZ_OUTPUT = UInt(64.W)// size of an output
-  def SZ_FLAG = UInt(1.W)
-
-  // Supported ALU functions
-  def FN_ADD  = 0.U
-  def FN_SUB  = 1.U
-  def FN_OR = 2.U
-  def FN_AND = 3.U
-  def FN_SLT  = 4.U
-  def FN_NOR = 5.U
-
-
-}
-
-import Alu._
+import Constants._
 /**
  * Class represent the ALU for the RISCV project.
  * Controlled by AluCtl.
  */
 class Alu() extends Module {
   val io = IO(new Bundle {
-    val fn = Input(SZ_ALU_FN) //ALU operation signal
-    val a = Input(SZ_INPUT) // Input A
-    val b = Input(SZ_INPUT) // Input B
-    val result = Output(SZ_OUTPUT) //Result of the ALU operation with A and B
-    val zeroFlag = Output(SZ_FLAG) // Zero flag; true if result is 0
+    val fn = Input(UInt(SZ_ALU_FN)) //ALU operation signal
+    val a = Input(UInt(SZ_INPUT)) // Input A
+    val b = Input(UInt(SZ_INPUT)) // Input B
+    val result = Output(UInt(SZ_OUTPUT)) //Result of the ALU operation with A and B
+    val zeroFlag = Output(UInt(SZ_FLAG)) // Zero flag; true if result is 0
   })
 
   //Use shorter variable names
   val fn = io.fn
   val a = io.a
   val b = io.b
-  val result = Wire(SZ_OUTPUT)
-  val zeroFlag = Wire(SZ_FLAG)
+  val result = Wire(UInt(SZ_OUTPUT))
+  val zeroFlag = Wire(UInt(SZ_FLAG))
 
   //Assign default values to outputs
   result := 0.U
