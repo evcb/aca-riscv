10dfa921a8105f428773f56420856d78ef8d7fb7
gabrielcobostello <gabrielcobtell@hotmail.com>
2020-04-04 20:42:52 +0200

    ALU control module and testbench

---
+++ src/test/scala/riscv/HazardDetectionUnitTest.scala
@@ -0,0 +1,59 @@
+package riscv
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest._
+
+object HazardDetectionUnitTester {
+  val param = Array("--target-dir", "generated", "--generate-vcd-output", "on")
+}
+
+
+class HazardDetectionUnitTester(dut: riscv.HazardDetectionUnit) extends PeekPokeTester(dut) {
+
+  //No NOP
+  poke(dut.io.IdExMemRead,false.B)
+  poke(dut.io.IdExRd, "b00001".U)
+  poke(dut.io.IfIdRs1, "b00001".U)
+  poke(dut.io.IfIdRs2, "b00001".U)
+  step(1)
+  expect(dut.io.NOP,true.B)
+
+  //No NOP
+  poke(dut.io.IdExMemRead,true.B)
+  poke(dut.io.IdExRd, "b00001".U)
+  poke(dut.io.IfIdRs1, "b00011".U)
+  poke(dut.io.IfIdRs2, "b00011".U)
+  step(1)
+  expect(dut.io.NOP,true.B)
+
+  //NOP because rd = rd1
+  poke(dut.io.IdExMemRead,true.B)
+  poke(dut.io.IdExRd, "b00001".U)
+  poke(dut.io.IfIdRs1, "b00001".U)
+  poke(dut.io.IfIdRs2, "b00011".U)
+  step(1)
+  expect(dut.io.NOP,false.B)
+
+  //NOP because rd = rd2
+  poke(dut.io.IdExMemRead,true.B)
+  poke(dut.io.IdExRd, "b00001".U)
+  poke(dut.io.IfIdRs1, "b00011".U)
+  poke(dut.io.IfIdRs2, "b00001".U)
+  step(1)
+  expect(dut.io.NOP,false.B)
+
+
+
+}
+
+class HazardDetectionUnitTest extends FlatSpec with Matchers {
+
+  "HazardDetectionUnit" should "pass" in {
+    iotesters.Driver.execute(HazardDetectionUnitTester.param,
+      () => new HazardDetectionUnit()) { c =>
+      new HazardDetectionUnitTester(c)
+    } should be(true)
+  }
+
+}
