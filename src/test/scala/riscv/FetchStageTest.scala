8314ff8d8092590d7173283cde8e93f16f3d9361
E <evscamilo@gmail.com>
2020-04-04 13:20:48 +0200

    Fixed FetchStage tests;

--- src/test/scala/riscv/FetchStageTest.scala
+++ src/test/scala/riscv/FetchStageTest.scala
@@ -19,7 +19,7 @@ class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
 
   step(1)
 
-  expect(r.io.ifOut, 8.U)
+  expect(r.io.ifOut, 1.U)
 
   poke(r.io.pcSrc, false.B)
   poke(r.io.ifIdPc, 0.U)
@@ -29,8 +29,7 @@ class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
 
   step(1)
 
-  // 10000000000000000000000000000000001
-  expect(r.io.ifOut, 17179869186L.U)
+  expect(r.io.ifOut, "b10000000000000000000000000000000010".U)
 
   // flushing
   poke(r.io.pcSrc, false.B)
@@ -41,8 +40,7 @@ class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
 
   step(1)
 
-  // 100000000000000000000000000000000000
-  expect(r.io.ifOut, 34359738368L.U)
+  expect(r.io.ifOut, "b100000000000000000000000000000000000".U)
 
   // incremental
   poke(r.io.pcSrc, false.B)
@@ -53,7 +51,6 @@ class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
 
   step(1)
 
-  // 110000000000000000000000000000000011
   expect(r.io.ifOut, "b110000000000000000000000000000000100".U)
 
   // branching
@@ -65,8 +62,18 @@ class FetchStageTester(r: FetchStage) extends PeekPokeTester(r) {
 
   step(2)
 
-  // 000000000000000000000000000000000001
   expect(r.io.ifOut, 1.U)
+
+  // incremental
+  poke(r.io.pcSrc, true.B)
+  poke(r.io.ifIdPc, 16.U)
+  poke(r.io.pcWrite, true.B)
+  poke(r.io.ifIdWrite, true.B)
+  poke(r.io.ifFlush, false.B)
+
+  step(2)
+
+  expect(r.io.ifOut, "b1000000000000000000000000000000000101".U)
 }
 
 class FetchStageTest extends FlatSpec with Matchers {
@@ -74,11 +81,11 @@ class FetchStageTest extends FlatSpec with Matchers {
   "FetchStage" should "pass" in {
     iotesters.Driver.execute(FetchStageTester.param,
       () => new FetchStage(Array(
-        "b00000000000000000000000000000001", // 0
+        "b00000000000000000000000000000001", // 0, 4, 8, 12, 16 ...
         "b00000000000000000000000000000010",
         "b00000000000000000000000000000011",
         "b00000000000000000000000000000100",
-        "b00000000000000000000000000000101"  // 16
+        "b00000000000000000000000000000101"
       ))) { c =>
       new FetchStageTester(c)
     } should be(true)
