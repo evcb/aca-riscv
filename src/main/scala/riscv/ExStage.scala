7267f282e8d53342a3eeda561c7e5a3460d39513
root <root@LAPTOP-PTBVC925.localdomain>
2020-04-07 17:05:21 +0200

    fix - 32-bit signals are signed

--- src/main/scala/riscv/ExStage.scala
+++ src/main/scala/riscv/ExStage.scala
@@ -18,8 +18,8 @@ class ExStage extends Module {
     val idExIn = Input(UInt(SZ_ID_EX_REG))  // stage input register ID/EX
     val idExCtlIn = Input(UInt(SZ_CTL_REG))  // stage input control register
 
-    val exMemAddr = Input(UInt(SZ_32_BIT)) // memory address from EX/MEM register
-    val memWbWd = Input(UInt(SZ_32_BIT)) // output from Mux in Writeback stage
+    val exMemAddr = Input(SInt(SZ_32_BIT)) // memory address from EX/MEM register
+    val memWbWd = Input(SInt(SZ_32_BIT)) // output from Mux in Writeback stage
     val exMemRegWrite = Input(Bool())//control signal for register file from EX/MEM register (used in Forwarder)
     val memWbRegWrite = Input(Bool())//control signal for register file from MEM/WB register (used in Forwarder)
     val exMemRd = Input(UInt(SZ_RD)) // register destination from EX/MEM register (used in Forwarder)
@@ -46,9 +46,9 @@ class ExStage extends Module {
   val aluSrc = io.idExCtlIn(ALU_SRC, 0).asBool() // 1 bit
   
   //Parse idExIn
-  val idExD1 = io.idExIn(ID_EX_D1, ID_EX_D2 + 1)// read data 1
-  val idExD2 = io.idExIn(ID_EX_D2, ID_EX_IMM + 1) //read data 2
-  val idExImm = io.idExIn(ID_EX_IMM, ID_EX_F + 1)  //immediate
+  val idExD1 = io.idExIn(ID_EX_D1, ID_EX_D2 + 1).asSInt// read data 1
+  val idExD2 = io.idExIn(ID_EX_D2, ID_EX_IMM + 1).asSInt //read data 2
+  val idExImm = io.idExIn(ID_EX_IMM, ID_EX_F + 1).asSInt  //immediate
   val idExF = io.idExIn(ID_EX_F, ID_EX_RS2 + 1) //func3 + func7
   val idExRs2 = io.idExIn(ID_EX_RS2, ID_EX_RS1 + 1)  // input to forwarder
   val idExRs1 = io.idExIn(ID_EX_RS1, ID_EX_RD + 1) //input to forwarder
@@ -60,12 +60,12 @@ class ExStage extends Module {
   /*********************************************************************************************************/
   val forwardA = Wire(UInt()) //output from forwarder
   val forwardB = Wire(UInt()) //output from forwarder
-  val aluResult = Wire(UInt()) //output from ALU
+  val aluResult = Wire(SInt()) //output from ALU
 
 
-  val outputMux1 = Wire(UInt(SZ_32_BIT)) // internal mux1 output
-  val outputMux2 = Wire(UInt(SZ_32_BIT)) // internal mux2 output
-  val outputMux3 = Wire(UInt(SZ_32_BIT)) // internal mux3 output
+  val outputMux1 = Wire(SInt(SZ_32_BIT)) // internal mux1 output
+  val outputMux2 = Wire(SInt(SZ_32_BIT)) // internal mux2 output
+  val outputMux3 = Wire(SInt(SZ_32_BIT)) // internal mux3 output
 
 
   /*********************************************************************************************************/
