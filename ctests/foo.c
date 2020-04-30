1ca78c14f10de4182750687dac743626b72b5339
root <root@LAPTOP-PTBVC925.localdomain>
2020-04-30 21:21:13 +0200

    script to generate things from .c files + generated things

--- ctests/foo.c
+++
@@ -1,9 +0,0 @@
-// This is our minimal startup code (usually in _start)
-
-int main() {
-
-  int a = 1;
-  int b = 2;
-
-  return a+b;
-}
---
+++ ctests/output/fibonacci/fibonacci.s
@@ -0,0 +1,68 @@
+	.file	"fibonacci.c"
+	.option nopic
+	.attribute arch, "rv32i2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+ #APP
+	addi sp, sp, 0x100
+	jal main
+	mv a1, a0
+	lui a0, 10
+	ecall
+ #NO_APP
+	.align	2
+	.globl	fib
+	.type	fib, @function
+fib:
+	addi	sp,sp,-32
+	sw	ra,28(sp)
+	sw	s0,24(sp)
+	sw	s1,20(sp)
+	addi	s0,sp,32
+	sw	a0,-20(s0)
+	lw	a4,-20(s0)
+	li	a5,1
+	bgt	a4,a5,.L2
+	lw	a5,-20(s0)
+	j	.L3
+.L2:
+	lw	a5,-20(s0)
+	addi	a5,a5,-1
+	mv	a0,a5
+	call	fib
+	mv	s1,a0
+	lw	a5,-20(s0)
+	addi	a5,a5,-2
+	mv	a0,a5
+	call	fib
+	mv	a5,a0
+	add	a5,s1,a5
+.L3:
+	mv	a0,a5
+	lw	ra,28(sp)
+	lw	s0,24(sp)
+	lw	s1,20(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	fib, .-fib
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sw	ra,28(sp)
+	sw	s0,24(sp)
+	addi	s0,sp,32
+	li	a5,4
+	sw	a5,-20(s0)
+	lw	a0,-20(s0)
+	call	fib
+	li	a5,0
+	mv	a0,a5
+	lw	ra,28(sp)
+	lw	s0,24(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (GNU) 9.2.0"
