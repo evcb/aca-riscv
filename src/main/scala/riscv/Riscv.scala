19ed1e99107d9afbf9d1e8c96f729566caca10d4
mariya <s193275@student.dtu.dk>
2020-05-03 19:26:11 +0200

    hacked uart from patmos

--- src/main/scala/riscv/Riscv.scala
+++ src/main/scala/riscv/Riscv.scala
@@ -1,172 +1,15 @@
 package riscv
 
 import chisel3._
-import chisel3.util.{Cat, DecoupledIO, Enum}
-
+import chisel3.util.{Cat, DecoupledIO, Enum, Queue}
+import scala.math._
 
 class UartIO extends DecoupledIO(UInt(8.W)) {
   override def cloneType: this.type = new UartIO().asInstanceOf[this.type]
 }
-
-
-/**
- * Transmit part of the UART.
- * A minimal version without any additional buffering.
- * Use a ready/valid handshaking.
- */
-class Tx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val txd = Output(UInt(1.W))
-    val channel = Flipped(new UartIO())
-  })
-
-  val BIT_CNT = ((frequency + baudRate / 2) / baudRate - 1).asUInt()
-
-  val shiftReg = RegInit(0x7ff.U)
-  val cntReg = RegInit(0.U(20.W))
-  val bitsReg = RegInit(0.U(4.W))
-
-  io.channel.ready := (cntReg === 0.U) && (bitsReg === 0.U)
-  io.txd := shiftReg(0)
-
-  when(cntReg === 0.U) {
-
-    cntReg := BIT_CNT
-    when(bitsReg =/= 0.U) {
-      val shift = shiftReg >> 1
-      shiftReg := Cat(1.U, shift(9, 0))
-      bitsReg := bitsReg - 1.U
-    }.otherwise {
-      when(io.channel.valid) {
-        shiftReg := Cat(Cat(3.U, io.channel.bits), 0.U) // two stop bits, data, one start bit
-        bitsReg := 11.U
-      }.otherwise {
-        shiftReg := 0x7ff.U
-      }
-    }
-
-  }.otherwise {
-    cntReg := cntReg - 1.U
-  }
-}
-
-/**
- * Receive part of the UART.
- * A minimal version without any additional buffering.
- * Use a ready/valid handshaking.
- *
- * The following code is inspired by Tommy's receive code at:
- * https://github.com/tommythorn/yarvi
- */
-class Rx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val rxd = Input(UInt(1.W))
-    val channel = new UartIO()
-  })
-
-  val BIT_CNT = ((frequency + baudRate / 2) / baudRate - 1).U
-  val START_CNT = ((3 * frequency / 2 + baudRate / 2) / baudRate - 1).U
-
-  // Sync in the asynchronous RX data, reset to 1 to not start reading after a reset
-  val rxReg = RegNext(RegNext(io.rxd, 1.U), 1.U)
-
-  val shiftReg = RegInit(0.U(8.W))
-  val cntReg = RegInit(0.U(20.W))
-  val bitsReg = RegInit(0.U(4.W))
-  val valReg = RegInit(false.B)
-
-  when(cntReg =/= 0.U) {
-    cntReg := cntReg - 1.U
-  }.elsewhen(bitsReg =/= 0.U) {
-    cntReg := BIT_CNT
-    shiftReg := Cat(rxReg, shiftReg >> 1)
-    bitsReg := bitsReg - 1.U
-    // the last shifted in
-    when(bitsReg === 1.U) {
-      valReg := true.B
-    }
-  }.elsewhen(rxReg === 0.U) { // wait 1.5 bits after falling edge of start
-    cntReg := START_CNT
-    bitsReg := 8.U
-  }
-
-  when(valReg && io.channel.ready) {
-    valReg := false.B
-  }
-
-  io.channel.bits := shiftReg
-  io.channel.valid := valReg
-}
-
-/**
- * A single byte buffer with a ready/valid interface
- */
-class Buffer extends Module {
-  val io = IO(new Bundle {
-    val in = Flipped(new UartIO())
-    val out = new UartIO()
-  })
-
-  val empty :: full :: Nil = Enum(2)
-  val stateReg = RegInit(empty)
-  val dataReg = RegInit(0.U(8.W))
-
-  io.in.ready := stateReg === empty
-  io.out.valid := stateReg === full
-
-  when(stateReg === empty) {
-    when(io.in.valid) {
-      dataReg := io.in.bits
-      stateReg := full
-    }
-  }.otherwise { // full
-    when(io.out.ready) {
-      stateReg := empty
-    }
-  }
-  io.out.bits := dataReg
-}
-
-/**
- * A transmitter with a single buffer.
- */
-class BufferedTx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val txd = Output(UInt(1.W))
-    val channel = Flipped(new UartIO())
-  })
-  val tx = Module(new Tx(frequency, baudRate))
-  val buf = Module(new Buffer())
-
-  buf.io.in <> io.channel
-  tx.io.channel <> buf.io.out
-  io.txd <> tx.io.txd
-}
-
-/**
- * Send a string.
- */
-class Sender(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val txd = Output(UInt(1.W))
-  })
-
-  val tx = Module(new BufferedTx(frequency, baudRate))
-
-  io.txd := tx.io.txd
-
-  val msg = "Hello World!"
-  val text = VecInit(msg.map(_.U))
-  val len = msg.length.U
-
-  val cntReg = RegInit(0.U(8.W))
-
-  tx.io.channel.bits := text(cntReg)
-  tx.io.channel.valid := cntReg =/= len
-
-  when(tx.io.channel.ready && cntReg =/= len) {
-    cntReg := cntReg + 1.U
-  }
+object log2Up
+{
+  def apply(in: Long): Int = if(in == 1) 1 else ceil(log(in)/log(2)).toInt
 }
 
 
@@ -245,17 +88,85 @@ class Riscv(data: Array[String] = Array(), frequency: Int = 50000000, baudRate: 
 
   //UART
   RegNext(io.rxd) // dont care - for now
-  val tx = Module(new BufferedTx(frequency, baudRate))
-  io.txd := tx.io.txd
-  io.led := 1.U
+  val clk_freq = 1000000000l
+  val baud_rate = 115200l
+  io.led := 1.U // TODO : remove
+  //val tx_baud_counter     = Reg(init = UInt(0, log2Up(clk_freq/baud_rate)))
 
-  val results = Reg(Vec(100, UInt(32.W)))
-  val cntReg = RegInit(0.U(8.W))
-  val cntBit = RegInit(0.U(8.W))
-  val cntWrd = RegInit(0.U(8.W))
+  val tx_baud_size = log2Up(clk_freq/baud_rate)
+  val calculated_baud = (clk_freq/baud_rate).asUInt()
+  val tx_baud_counter     = RegInit(0.U (tx_baud_size.W))
+  val tx_baud_tick        = RegInit(0.U (1.W))
 
-  results(cntReg) := memWbData
-  /*
+  // UART TX clk
+    when (tx_baud_counter === calculated_baud){
+      tx_baud_counter     := 0.U
+      tx_baud_tick        := 1.U
+    }
+    .otherwise {
+      tx_baud_counter     := tx_baud_counter + 1.U
+      tx_baud_tick        := 0.U
+    }
+
+  //create a Queue with 32 items of 8 bytes each
+  val txQueue = Module(new Queue(Bits(32.W), 8))
+  txQueue.io.enq.bits := decodeStage.io.MemWbWd
+  txQueue.io.enq.valid := false.B
+  txQueue.io.deq.ready := false.B
+
+
+  //create states to process queue
+  val tx_idle :: tx_send :: Nil = Enum(2)
+  val tx_state = RegInit(tx_idle)
+  val tx_buff = RegInit(0.U (10.W))
+  val tx_reg = RegInit(1.U (1.W))
+  val tx_counter = RegInit(0.U (4.W))
+  val UART_REGISTER = 0.U; // for now, register 0
+
+  //write to queue every clock
+  when(decodeStage.io.MemWbRd === UART_REGISTER)
+  {
+    txQueue.io.enq.bits := decodeStage.io.MemWbWd
+    txQueue.io.enq.valid := true.B
+  }
+
+  // process queue - prepare for send
+  when (tx_state === tx_idle) {
+
+    when (txQueue.io.deq.valid) {
+      txQueue.io.deq.ready := true.B
+      tx_buff := Cat(true.B, txQueue.io.deq.bits, false.B)
+      tx_state := tx_send
+    }
+  }
+
+  // send data prepared
+  when (tx_state === tx_send) {
+    when (tx_baud_tick === 1.U){
+      tx_buff         := Cat (0.U, tx_buff (9, 1))
+      tx_reg          := tx_buff(0)
+      tx_counter      := Mux(tx_counter === 10.U, 0.U, tx_counter + 1.U)
+
+      when (tx_counter === 10.U) {
+        when (txQueue.io.deq.valid) {
+          txQueue.io.deq.ready := true.B
+          tx_buff              := Cat(true.B, txQueue.io.deq.bits)
+          tx_reg               := 0.U
+          tx_counter           := 1.U
+        }
+          .otherwise {
+            tx_reg          := 1.U
+            tx_counter      := 0.U
+            tx_state        := tx_idle
+          }
+      }
+    }
+  }
+
+  // Connect TX pin
+  io.txd := tx_reg
+
+ /*
     cntReg := cntReg + 1.U
 
     val value = results(cntWrd)
@@ -270,20 +181,18 @@ class Riscv(data: Array[String] = Array(), frequency: Int = 50000000, baudRate: 
         cntBit := 0.U
       }
     }*/
-  val msg = "Hello World!"
-  val text = VecInit(msg.map(_.U))
-  val len = msg.length.U
+  //
 
-/*
-  val cntReg = RegInit(0.U(8.W))
-*/
+/*  val msg = "Hello World!"
+  val text = VecInit(msg.map(_.U))*/
+/*  val len = 32.U
 
-  tx.io.channel.bits := text(cntReg)
+  tx.io.channel.bits := results(cntReg)
   tx.io.channel.valid := cntReg =/= len
 
   when(tx.io.channel.ready && cntReg =/= len) {
     cntReg := cntReg + 1.U
-  }
+  }*/
 //
 //  printf("- Start of cycle %d: \n", (pc / 4.S))
 //  printf("------------------------------\n")
