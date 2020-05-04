39d9ac74eadf99919c99c4f1998d7451afa5c907
root <root@LAPTOP-PTBVC925.localdomain>
2020-05-04 20:46:26 +0200

    add function and test for add function

--- ctests/add.c
+++ ctests/add.c
@@ -1,9 +1,6 @@
-asm("addi sp, sp, 0x100"); // SP set to 256
+asm("addi sp, sp, 0x200"); // SP set to 512
 asm("jal main");        // call main
 asm("mv a1, a0");       // save return value in a1
-asm("lui a0, 10");       // prepare ecall exit
-asm("ecall");           // now your simulator should stop
-
 int main() {
 
   int a = 1;
