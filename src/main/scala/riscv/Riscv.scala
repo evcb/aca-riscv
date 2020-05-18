0a7afcbe5ee28fd5437fcc693203b601c17830ff
mariya <s193275@student.dtu.dk>
2020-05-18 12:31:23 +0200

    took uart module out of main risc file

--- src/main/scala/riscv/Riscv.scala
+++ src/main/scala/riscv/Riscv.scala
@@ -3,147 +3,6 @@ package riscv
 import chisel3._
 import chisel3.util.{Cat, DecoupledIO, Enum}
 
-
-class UartIO extends DecoupledIO(UInt(8.W)) {
-  override def cloneType: this.type = new UartIO().asInstanceOf[this.type]
-}
-
-
-/**
- * Transmit part of the UART.
- * A minimal version without any additional buffering.
- * Use a ready/valid handshaking.
- */
-class Tx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val txd = Output(UInt(1.W))
-    val channel = Flipped(new UartIO())
-  })
-
-  val BIT_CNT = ((frequency + baudRate / 2) / baudRate - 1).asUInt()
-
-  val shiftReg = RegInit(0x7ff.U)
-  val cntReg = RegInit(0.U(20.W))
-  val bitsReg = RegInit(0.U(4.W))
-
-  io.channel.ready := (cntReg === 0.U) && (bitsReg === 0.U)
-  io.txd := shiftReg(0)
-
-  when(cntReg === 0.U) {
-
-    cntReg := BIT_CNT
-    when(bitsReg =/= 0.U) {
-      val shift = shiftReg >> 1
-      shiftReg := Cat(1.U, shift(9, 0))
-      bitsReg := bitsReg - 1.U
-    }.otherwise {
-      when(io.channel.valid) {
-        shiftReg := Cat(Cat(3.U, io.channel.bits), 0.U) // two stop bits, data, one start bit
-        bitsReg := 11.U
-      }.otherwise {
-        shiftReg := 0x7ff.U
-      }
-    }
-
-  }.otherwise {
-    cntReg := cntReg - 1.U
-  }
-}
-
-/**
- * Receive part of the UART.
- * A minimal version without any additional buffering.
- * Use a ready/valid handshaking.
- *
- * The following code is inspired by Tommy's receive code at:
- * https://github.com/tommythorn/yarvi
- */
-class Rx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val rxd = Input(UInt(1.W))
-    val channel = new UartIO()
-  })
-
-  val BIT_CNT = ((frequency + baudRate / 2) / baudRate - 1).U
-  val START_CNT = ((3 * frequency / 2 + baudRate / 2) / baudRate - 1).U
-
-  // Sync in the asynchronous RX data, reset to 1 to not start reading after a reset
-  val rxReg = RegNext(RegNext(io.rxd, 1.U), 1.U)
-
-  val shiftReg = RegInit(0.U(8.W))
-  val cntReg = RegInit(0.U(20.W))
-  val bitsReg = RegInit(0.U(4.W))
-  val valReg = RegInit(false.B)
-
-  when(cntReg =/= 0.U) {
-    cntReg := cntReg - 1.U
-  }.elsewhen(bitsReg =/= 0.U) {
-    cntReg := BIT_CNT
-    shiftReg := Cat(rxReg, shiftReg >> 1)
-    bitsReg := bitsReg - 1.U
-
-    // the last shifted in
-    when(bitsReg === 1.U) {
-      valReg := true.B
-    }
-  }.elsewhen(rxReg === 0.U) { // wait 1.5 bits after falling edge of start
-    cntReg := START_CNT
-    bitsReg := 8.U
-  }
-
-  when(valReg && io.channel.ready) {
-    valReg := false.B
-  }
-
-  io.channel.bits := shiftReg
-  io.channel.valid := valReg
-}
-
-/**
- * A single byte buffer with a ready/valid interface
- */
-class Buffer extends Module {
-  val io = IO(new Bundle {
-    val in = Flipped(new UartIO())
-    val out = new UartIO()
-  })
-
-  val empty :: full :: Nil = Enum(2)
-  val stateReg = RegInit(empty)
-  val dataReg = RegInit(0.U(8.W))
-
-  io.in.ready := stateReg === empty
-  io.out.valid := stateReg === full
-
-  when(stateReg === empty) {
-    when(io.in.valid) {
-      dataReg := io.in.bits
-      stateReg := full
-    }
-  }.otherwise { // full
-    when(io.out.ready) {
-      stateReg := empty
-    }
-  }
-  io.out.bits := dataReg
-}
-
-/**
- * A transmitter with a single buffer.
- */
-class BufferedTx(frequency: Int, baudRate: Int) extends Module {
-  val io = IO(new Bundle {
-    val txd = Output(UInt(1.W))
-    val channel = Flipped(new UartIO())
-  })
-  val tx = Module(new Tx(frequency, baudRate))
-  val buf = Module(new Buffer())
-
-  buf.io.in <> io.channel
-  tx.io.channel <> buf.io.out
-  io.txd <> tx.io.txd
-}
-
 class Riscv(data: Array[String] = Array(), frequency: Int = 50000000, baudRate: Int = 115200) extends Module {
   val io = IO(new Bundle {
     val rxd = Input(UInt(1.W))
@@ -185,8 +44,14 @@ class Riscv(data: Array[String] = Array(), frequency: Int = 50000000, baudRate: 
   val executionStage = Module(new ExStage())
   val memStage = Module(new MemStage())
   val writeBackStage = Module(new WriteBackStage())
+  val uart = Module(new UartMain(frequency, baudRate))
 
   // Wiring
+  //UART
+  uart.io.rxd := io.rxd
+  uart.io.memWbRd := decodeStage.io.MemWbRd
+  uart.io.memWbData := decodeStage.io.MemWbWd
+  io.txd := uart.io.txd
 
   // IF
   fetchStage.io.pcSrc := decodeStage.io.pcSrc
@@ -246,24 +111,8 @@ class Riscv(data: Array[String] = Array(), frequency: Int = 50000000, baudRate: 
   memWbData := decodeStage.io.MemWbWd
   memWbRd := decodeStage.io.MemWbRd
 
-  //UART
-  RegNext(io.rxd) // dont care - for now
-  val tx = Module(new BufferedTx(frequency, baudRate))
-  io.txd := tx.io.txd
+  //turn on LED -> program is running
   io.led := 1.U
-  val cntReg = RegInit(0.U(8.W))
-  val dtReg = RegInit(0.U(32.W))
-
-  when(memWbRd === 10.U && dtReg === 0.U && memWbData =/= 0.U) {
-    dtReg := memWbData
-  }
-
-  tx.io.channel.bits := dtReg
-  tx.io.channel.valid := cntReg <= 50.U
-
-  when(tx.io.channel.ready && cntReg <= 50.U){
-    cntReg := cntReg + 1.U
-  }
 
   printf("- Start of cycle %d: \n", (pc / 4.S))
   printf("------------------------------\n")
